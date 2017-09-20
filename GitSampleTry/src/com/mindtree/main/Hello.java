cd54094e0a1a19dfec9a6fc84a58c39ed29f2310
M1039304 <M1039304@A2MD20779.mindtree.com>
2017-09-20 15:43:19 +0530

    trying to commit

---
+++ GitSampleTry/src/com/mindtree/main/Hello.java
@@ -0,0 +1,11 @@
+package com.mindtree.main;
+
+public class Hello {
+	
+	public static void main(String args[]){
+		
+		System.out.println("Hello everyone");
+		System.out.println("This is an sample Java application to experiment deployment of an simple application on git");
+	}
+
+}
